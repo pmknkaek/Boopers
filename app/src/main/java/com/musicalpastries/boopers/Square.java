0c8826442e22b9e11b68934a43f7579ee7a23d6b
pmknkaek <cakesokorigins@gmail.com>
2017-09-26 21:51:24 -0600

    Fixed square drawing

--- app/src/main/java/com/musicalpastries/boopers/Square.java
+++ app/src/main/java/com/musicalpastries/boopers/Square.java
@@ -40,9 +40,9 @@ public class Square {
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
     static float squareCoords[] = {
+            -0.5f, -0.5f, 0.0f,  // bottom left
             -0.5f,  0.5f, 0.0f,   // top left
-            -0.5f, -0.5f, 0.0f,   // bottom left
-            0.5f, -0.5f, 0.0f,   // bottom right
+            0.5f, -0.5f, 0.0f, // bottom right
             0.5f,  0.5f, 0.0f }; // top right
 
     private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
@@ -107,8 +107,7 @@ public class Square {
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
 
         // Draw the square
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 1, vertexCount);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, vertexCount);
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
